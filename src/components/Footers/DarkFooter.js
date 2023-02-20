21ec4a817aeff165262ab1ba2eddf366880ca4fe
musense <musense.ad@gmail.com>
2023-02-20 15:08:16 +0800

    modify footer margin checked by pathname

--- src/components/Footers/DarkFooter.js
+++ src/components/Footers/DarkFooter.js
@@ -1,5 +1,7 @@
 /*eslint-disable*/
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
+import { useLocation } from "react-router-dom";
+import Tag from "views/index-sections/Tag";
 
 // reactstrap components
 import styles from "./darkFooter.module.css";
@@ -7,68 +9,59 @@ import styles from "./darkFooter.module.css";
 
 function DarkFooter({ tags }) {
 
-
-  const mounted = useRef();
+  const location = useLocation()
   const [firstRowTags, setFirstRowTags] = useState(null);
   const [secondRowTags, setSecondRowTags] = useState(null);
   const [thirdRowTags, setThirdRowTags] = useState(null);
 
+  const [marginClassName, setMarginClassName] = useState("");
+  const getMarginCSS = () => useMemo(() => {
+    if (location.pathname === "/") {
+      setMarginClassName("index-margin")
+    } else if (location.pathname.indexOf('/content/tag') !== -1) {
+      setMarginClassName("tag-margin")
+    } else {
+      setMarginClassName("content-margin")
+    }
+  }, [location.key])
+
+  getMarginCSS()
+
+  const setTagRows = (start = 0, end = arr.length, arr = []) => {
+    if (arr === null) return
+    if (arr.length === 0) return
+    return arr.slice(start, end)
+  }
 
   useEffect(() => {
-
-    setFirstRowTags(tags ? tags.slice(0, 4) : null)
-    setSecondRowTags(tags ? tags.slice(4, 7) : null)
-    setThirdRowTags(tags ? tags.slice(7, 11) : null)
-
+    setFirstRowTags(setTagRows(0, 4, tags))
+    setSecondRowTags(setTagRows(4, 7, tags))
+    setThirdRowTags(setTagRows(7, 11, tags))
   }, [tags]);
 
   return (
-    <footer id="footer" className={`footer ${styles['custom-footer']}`} >
+    <footer id="footer" className={`footer ${styles['custom-footer']} ${styles[marginClassName]}`} >
       {firstRowTags || secondRowTags || thirdRowTags
         ? (
           <nav className={styles['custom-nav']}>
             <ul className={styles['custom-ul']}>
               {firstRowTags.map((tag, index) => (
                 <li className={styles['custom-li']} key={index}>
-                  {/* <Link
-                    key={index}
-                    to={`/index/${tag._id}`}
-                    target="_blank"
-                  > */}
-                  <div>
-                    # {tag.name}
-                  </div>
-                  {/* </Link> */}
+                  <Tag index={index} tagName={tag.name} />
                 </li>
               ))}
             </ul>
             <ul className={styles['custom-ul']}>
               {secondRowTags.map((tag, index) => (
                 <li className={styles['custom-li']} key={index}>
-                  {/* <Link
-                    key={index}
-                    to={`/index/${tag._id}`}
-                    target="_blank"
-                  > */}
-                  <div>
-                    # {tag.name}
-                  </div>
-                  {/* </Link> */}
+                  <Tag index={index} tagName={tag.name} />
                 </li>
               ))}
             </ul>
             <ul className={styles['custom-ul']}>
               {thirdRowTags.map((tag, index) => (
                 <li className={styles['custom-li']} key={index}>
-                  {/* <Link
-                    key={index}
-                    to={`/index/${tag._id}`}
-                    target="_blank"
-                  > */}
-                  <div>
-                    # {tag.name}
-                  </div>
-                  {/* </Link> */}
+                  <Tag index={index} tagName={tag.name} />
                 </li>
               ))}
             </ul>
